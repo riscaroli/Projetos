40c82af1e89c46770580d8e2dde718e05603bffe
riscaroli <riscarolli.gabriel17@gmail.com>
2022-12-06 18:39:56 -0300

    Projeto

---
+++ Java/script.js
@@ -0,0 +1,37 @@
+// Console
+
+console.error("Este é o erro!")
+
+console.log("Meu nome é Gabriel!")
+
+console.warn("Este é um aviso!")
+
+// Variáveis
+
+var x = 10    //<-- Pode mudar de valor
+
+// Maneiras modernas/melhores
+
+let y = 15    //<-- Pode mudar de valor
+
+const z = 20  //<-- Não muda de valor de preferência este
+
+// Tipos de dados
+
+const name = "Gabriel"
+
+const true_or_false = true
+
+let surprise = null
+
+let age //<-- Incorreto
+
+console.log(typeof name) //<-- Validação
+
+console.log(typeof z) //<-- Validação
+
+console.log(typeof true_or_false) //<-- Validação
+
+console.log(typeof surprise) //<-- Validação
+
+console.log(typeof age) //<-- Validação
