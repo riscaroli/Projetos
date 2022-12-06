55fb1ade963d0bb099890313fda80f3aafb738cf
riscaroli <riscarolli.gabriel17@gmail.com>
2022-12-06 19:57:20 -0300

    Projeto

--- Java/script.js
+++ Java/script.js
@@ -44,4 +44,103 @@ console.log(typeof lista) //<-- Validação Array
 
 console.log(typeof usuario) //<-- Validação Object literals
 
-// Métodos de string
+// Métodos de String
+
+const nome_completo = "Gabriel Ari Riscarolli"
+
+console.log(nome_completo.length)  //<-- Contagem de caractéries
+
+console.log(nome_completo.toLocaleLowerCase())  // Muda a letra para minúscula
+
+console.log(nome_completo.toLocaleUpperCase())  // Muda a letra para maiúscula
+
+const nome_lista = nome_completo.split(" ")  //<-- Separação com caractéres
+
+console.log(nome_lista)
+
+console.log(nome_completo.indexOf("Riscarolli"))  // Número de Caractéries até a palavra escrita
+
+console.log(nome_completo.slice(0, 11)) // Extrair uma parte de uma string
+
+// Métodos de Arrays
+
+const list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+list[10] = 11
+
+const[n1, n2] = list  //<-- Extrai a váriavel da lista
+
+console.log(list.length)  //<-- Contagem de elementos
+
+console.log(list[2])  //<-- Extrair um elemento específico do array
+
+console.log(list)
+
+console.log(list[list.length - 1 ])  //<-- Pegar o último elemento da lista
+
+list.push(12)   // Add um elemento
+
+console.log(list)
+
+list.pop() // Remove o elemento final do array
+
+console.log(list)
+
+list.shift()  //Remove o primeiro elemento
+
+list.unshift()  // Add o primeiro elemento
+
+// Objetos
+
+const produtos = {
+    Nome: "Kombi",
+    Preço: 8000,
+    Estoque: true, 
+    Ano: ["1970","1980","1990"],
+}
+console.log(produtos.Nome) // Igual a console.log(produtos["Nome"])
+
+// Destructuring
+
+const{Preço, Estoque} = produtos  //<-- Extrai a váriavel da lista
+
+console.log(Preço)
+
+console.log(Estoque)
+
+// JSON - JavaScript Object Notation 
+
+const dog = {
+    Name: "Shark", Age: 10
+}
+const json = JSON.stringify(dog) // Coverter para JSON
+
+console.log(json)
+
+const obj = JSON.parse(json)  // Converter para o JS
+
+console.log(obj)
+
+// Estruturas Condicionais 
+
+const a = 10
+const b = "Gabriel"
+
+if(a > 9){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
+
+// "==" Valor da Váriavel é igual e "===" Valor e tipo são identicos
+
+if(b === "Marcos"){
+    console.log("True")
+}
+else if(b === "Jonathan"){
+    console.log("True")
+}
+else{
+    console.log("False")
+}
